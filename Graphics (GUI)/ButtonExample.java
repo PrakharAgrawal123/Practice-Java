506487b6bc7608171d6777201ce6fd45e7f47ded
Prakhar Agrawal <agrawalprakhar931@gmail.com>
2025-11-05 01:11:15 +0530

    Added ButtonExample

---
+++ Graphics (GUI)/ButtonExample.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class ButtonExample {
+  public static void main(String[] args) {
+    Frame frame = new Frame("Button Example");
+    Button button = new Button("Click Me");
+    button.setBounds(50, 100, 80, 30);
+    // Add ActionListener to the button
+    button.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        System.out.println("Button clicked");
+      }
+    });
+    frame.add(button);
+    frame.setSize(300, 300);
+    frame.setLayout(null); // Using no layout managers
+    frame.setVisible(true);
+  }
+}
