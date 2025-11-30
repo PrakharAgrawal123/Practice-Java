efc17121a83f0593eafce91fc28addd0687995d1
Prakhar Agrawal <agrawalprakhar931@gmail.com>
2025-11-30 19:33:29 +0530

    Update PalindromeCheck

--- String/PalindromeCheck.java
+++ String/PalindromeCheck.java
@@ -1,24 +1,20 @@
-public class PalindromeCheck {
+import java.util.*;
+public class PalindromeCheck{
   public static void main(String[] args) {
-    String str = "madam";
-
-    int start = 0;
-    int end = str.length() - 1;
-
-    boolean isPalindrome = true;
-
-    while (start < end) {
-      if (str.charAt(start) != str.charAt(end)) {
-        isPalindrome = false;
-        break;
+      Scanner sc  = new Scanner(System.in);
+      System.out.println("Enter a String:");
+      String n = sc.nextLine();
+      
+      String rev = "";
+      for (int i = n.length() - 1; i >= 0; i--) {
+        rev = rev + n.charAt(i);
       }
-      start++;
-      end--;
-    }
 
-    if (isPalindrome)
-      System.out.println("Palindrome");
-    else
-      System.out.println("Not Palindrome");
+      if (rev.equals(n)) {
+        System.out.println("String is Palindrome");
+      }  
+      else {
+        System.out.println("String is not Palindrome");
+      }
   }
 }
